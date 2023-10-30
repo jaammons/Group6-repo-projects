4a5f4ed684845bfd61af7755841a2b305c55eda6
Sean Norini <141059916+SeanNorini@users.noreply.github.com>
2023-10-30 16:06:40 -0400

    Added Django test server for selenium tests and instructions in README.md

--- ecommerce/tests/test_users.py
+++ ecommerce/tests/test_users.py
@@ -1,45 +1,62 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import pytest
 import time
 
-@pytest.fixture
-def driver():
-    """
-    Returns a ChromeDriver with index.html open.
-    """
-    driver = webdriver.Chrome()
-    driver.get("http://127.0.0.1:8000/")
-    yield driver
 
-@pytest.fixture
-def admin_driver():
-    """
-    Returns a ChromeDriver logged in as Admin on index.html.
-    """
-    driver = webdriver.Chrome()
-    driver.get("http://127.0.0.1:8000/users/login")
-    driver.find_element(By.NAME, "username").send_keys("Admin")
-    driver.find_element(By.NAME, "password").send_keys("Admin")
-    driver.find_element(By.NAME, "login").click()
-    yield driver
 
-def test_login(admin_driver):
-    """
-    Test verifies admin_driver log in was successful by checking user greeting
-    on index.html after log in attempt.
-    Expected result: Admin is logged in.
-    """
-    greeting = admin_driver.find_element(By.ID, "greeting")
-    assert greeting.text == "Welcome, Admin."
+class UserTests(StaticLiveServerTestCase):  
+    fixtures = ["testusers.json"]
 
-def test_logout(admin_driver):
-    """
-    Test verifies log out was successful by checking user greeting
-    on index.html after log out attempt.
-    Expected result: User is logged out.
-    """
-    admin_driver.find_element(By.ID, "logout").click()
-    greeting = admin_driver.find_element(By.ID, "greeting")
-    assert greeting.text == "Not signed in."
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    
+    def login_admin(self):
+        """
+        Returns a ChromeDriver logged in as Admin on index.html.
+        """
+        self.selenium.get(f"{self.live_server_url}/users/login")
+        self.selenium.find_element(By.NAME, "username").send_keys("Admin")
+        self.selenium.find_element(By.NAME, "password").send_keys("Admin")
+        self.selenium.find_element(By.NAME, "login").click()
+        
+
+    def test_login(self):
+        """
+        Test verifies login_admin was successful by checking user greeting
+        on index.html after log in attempt.
+        Expected result: Admin is logged in.
+        """
+        self.selenium.get(f"{self.live_server_url}/users/login")
+        time.sleep(2)
+        self.selenium.find_element(By.NAME, "username").send_keys("Admin")
+        self.selenium.find_element(By.NAME, "password").send_keys("Admin")
+        time.sleep(2)
+        self.selenium.find_element(By.NAME, "login").click()
+        time.sleep(2)
+        greeting = self.selenium.find_element(By.ID, "greeting")
+        assert greeting.text == "Welcome, Admin."
+
+    def test_logout(self):
+        """
+        Test verifies logout was successful by checking user greeting
+        on index.html after log out attempt.
+        Expected result: User is logged out.
+        """
+        self.login_admin()
+        time.sleep(2)
+        self.selenium.find_element(By.ID, "logout").click()
+        time.sleep(2)
+        greeting = self.selenium.find_element(By.ID, "greeting")
+        assert greeting.text == "Not signed in."
     
