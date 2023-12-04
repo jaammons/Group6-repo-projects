53d6dcc31cc1cd904706e309ea7430e858cb78a6
Sean Norini <141059916+SeanNorini@users.noreply.github.com>
2023-12-04 13:46:03 -0500

    Updated pytest selenium fixtures to skip modules not using selenium

--- ecommerce/tests/conftest.py
+++ ecommerce/tests/conftest.py
@@ -3,6 +3,8 @@ from utilities import capture_screenshot
 from selenium import webdriver
 import pytest                               
 
+SKIP_MODULES = ["test_api"]
+
 def pytest_addoption(parser) -> None:
     parser.addoption("--screenshot", action="store_true", default=False, help="Takes a screenshot at the end of each test.")
 
@@ -14,24 +16,30 @@ def django_db_setup(django_db_setup, django_db_blocker):
       call_command("flush", interactive=False)
       call_command('loaddata', 'default.json')
 
-@pytest.fixture(scope="session")
-def driver(live_server) -> webdriver.Chrome:
-   # Create chromedriver instance
-   driver = webdriver.Chrome()
-   yield driver
-   # End chromedriver instance
-   driver.quit()
+@pytest.fixture(scope="module")
+def driver(live_server, request) -> webdriver.Chrome:
+    if request.module.__name__ in SKIP_MODULES:
+        yield
+    else:
+        # Create chromedriver instance
+        driver = webdriver.Chrome()
+        yield driver
+        # End chromedriver instance
+        driver.quit()
 
 @pytest.fixture(scope="function", autouse=True)
 def screenshot(request, driver):
-    yield
-    # Get the name of the test function
-    function_name = request.function.__name__
+    if request.module.__name__ in SKIP_MODULES:
+        yield
+    else:
+        yield
+        # Get the name of the test function
+        function_name = request.function.__name__
 
-    # Take screenshot if --screenshot in command line arguments
-    if request.config.getoption("--screenshot"):
-        capture_screenshot(driver, function_name)
+        # Take screenshot if --screenshot in command line arguments
+        if request.config.getoption("--screenshot"):
+            capture_screenshot(driver, function_name)
 
-    # Take screenshot if test failed
-    elif not request.session.testsfailed:
-        capture_screenshot(driver, function_name + "_actual")
+        # Take screenshot if test failed
+        elif not request.session.testsfailed:
+            capture_screenshot(driver, function_name + "_actual")
